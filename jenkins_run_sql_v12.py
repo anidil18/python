b6e8efdf6b1b8f47b23402cc9d61b78f48e6bb8a
Jiri Srba <jiri.srba@gmail.com>
2017-11-18 18:52:29 +0100

    Add logger Add read YAML CI config Change convert do dict cfg

--- jenkins_run_sql_v12.py
+++ jenkins_run_sql_v12.py
@@ -6,8 +6,6 @@ Jenkins build script to execute SQL file
 USERNAME = / + connect string z REST API INFP
 
 export TNS_ADMIN=/etc/oracle/wallet/sys
-
-export APP=ORDBF
 export SQLCL=sqlplus
 """
 
@@ -16,6 +14,9 @@ from __future__ import print_function
 
 import argparse
 import os
+import sys
+import yaml
+import logging
 from subprocess import Popen, PIPE
 import requests
 from requests.auth import HTTPBasicAuth
@@ -24,6 +25,11 @@ from requests.auth import HTTPBasicAuth
 __version__ = '1.2'
 __author__ = 'Jiri Srba'
 __status__ = 'Development'
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
 
 # INFP Rest API
 INFP_REST_OPTIONS = {
@@ -36,18 +42,32 @@ JIRA_REST_OPTIONS = {
     'server': 'https://jira.atlassian.com'}
 
 # Oracle wallet for SYS
-TNS_ADMIN_SYS = '/etc/oracle/wallet/sys'
+TNS_ADMIN_DIR = '/etc/oracle/wallet'
 
 def get_jira_issue(jira_rest_options, jira_issue):
   """Get info from JIRA ticket"""
   pass
 
 
+def read_ci_config(config_file):
+  """Read YAML config file"""
+
+  with open(config_file) as stream:
+    try:
+      cfg = yaml.load(stream)
+    except yaml.YAMLError as exc:
+      logger.error(exc)
+      raise
+  return cfg
+
 def get_db_info(infp_rest_options, dbname):
   """Rest API call to INFP"""
 
   # CA cert file
-  verify = '/etc/ssl/certs/ca-bundle.crt'
+  if sys.platform == "linux":
+    verify = '/etc/ssl/certs/ca-bundle.crt'
+  else:
+    verify = False
 
   r = requests.get(
       '/'.join([infp_rest_options['url'], dbname]),
@@ -65,7 +85,7 @@ def get_db_info(infp_rest_options, dbname):
 
 def check_for_app(dbinfo, app):
   """Kontrola, zda je databaze registrovana v OLI pro danou APP"""
-  if app not in dbinfo['app_name']:
+  if app.upper() not in dbinfo['app_name']:
     raise ValueError(
         'Databaze {db} neni registrovana pro aplikaci {app}.'
         .format(db=dbinfo['dbname'], app=app))
@@ -74,62 +94,107 @@ def check_for_app(dbinfo, app):
 def check_for_env_status(dbinfo):
   """Kontrola, zda neni database registrovana jako produkcni"""
   if 'Production' in dbinfo['env_status']:
-    raise ValueError(
+    raise AssertionError(
         'Databaze {} je registrovana jako produkcni.'.format(dbinfo))
 
 
-def run_sql_script(sqlcl, connect_string, sql_filename):
+def run_sql_script(sqlcl_string, sql_file):
   """Run SQL script with connect description"""
 
-  sqlplus = Popen([sqlcl, connect_string], stdin=PIPE,
+  sqlplus = Popen(sqcl_string.split(), stdin=PIPE,
                   stdout=PIPE, stderr=PIPE, universal_newlines=True)
-  sqlplus.stdin.write('@' + sql_filename)
+  sqlplus.stdin.write('@' + sql_file)
   return sqlplus.communicate()
 
 
-def main(dbname, sql_script, jira_issue):
+def main(args):
   """ Main function """
 
-  # nacteni sql filename z argv[1]
-  sql_filename = ' '.join(sql_script)
+  cfg = {
+      'variables': {
+          'dbname': None,
+          'app': None,
+          'username': 'SYS'},
+      'stage': ['deploy'],
+      'script': [],
+      'jira': None}
 
-  dbinfo = get_db_info(INFP_REST_OPTIONS, dbname)
-  print('dbinfo: {}'.format(dbinfo))
-  print('sql script file: {}'.format(sql_filename))
+  logger.debug('args: %s', args)
+  logger.debug('cfg: %s', cfg)
+
+  # parse config file
+  if args.config_file:
+    cfg = read_ci_config(args.config_file)
+    logging.debug('cfg: %s', cfg)
+
+  # overwrite dbname
+  if args.dbname:
+    cfg['variables']['dbname'] = args.dbname
+
+  # nacteni sql filename z argv[1]
+  if args.script:
+    cfg['script'] = [' '.join(args.script)]
+
+  if args.jira_issue:
+    cfg['jira'] = args.jira_issue
+    logging.debug('jira: %s', cfg['jira'])
+
+  if not cfg['variables']['dbname']:
+    raise AssertionError("Value dbname not specified")
+
+  dbinfo = get_db_info(INFP_REST_OPTIONS, cfg['variables']['dbname'])
+  logging.info('dbinfo: %s', dbinfo)
+  logging.info('sql script file: %s', cfg['script'])
 
   # check for production env
   check_for_env_status(dbinfo)
 
   # check for APP if defined
-  if 'APP' in os.environ:
-    app = os.environ['APP']
-    check_for_app(dbinfo, app)
+  if args.app_name:
+    cfg['variables']['app'] = args.app_name
 
+  if cfg['variables']['app']:
+    check_for_app(dbinfo, args.app_name)
+
+  if args.username:
+    cfg['variables']['username'] = args.username.upper()
+
+  # read ENV config variables
   if 'SQLCL' in os.environ:
-    sqlcl = os.environ['SQLCL']
+    sqlcl_string = os.environ['SQLCL']
   else:
-    sqlcl = 'sqlplus'
+    sqlcl_string = 'sqlplus'
 
   if 'TNS_ADMIN' not in os.environ:
-    os.environ['TNS_ADMIN'] = TNS_ADMIN_SYS
+    os.environ['TNS_ADMIN'] = os.path.join(
+        TNS_ADMIN_DIR, lower(cfg['variables']['username']))
 
-  connect_string = '/@//' + dbinfo['connect_descriptor'] + ' AS SYSDBA'
+  sqlcl_string += '/@//' + dbinfo['connect_descriptor']
+  if 'SYS' in cfg['variables']['username']:
+    sqlcl_string += ' AS SYSDBA'
 
-  sql_result = run_sql_script(sqlcl, connect_string, sql_filename)
-  if sql_result:
-    # print sqlplus result with newlines
-    for line in sql_result:
-      print(line, end='')
+  for f in cfg['script']:
+    result = run_sql_script(sqlcl_string, f)
+    if result:
+      # print sqlplus result with newlines
+      for line in result:
+        print(line, end='')
 
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(
       description="Jenkins build script to execute SQL file")
-  parser.add_argument('sql_script', metavar='sql_script', type=str, nargs='+',
-                      help='SQL script to execute')
   parser.add_argument('-d', '--db', action="store", dest="dbname",
-                      required=True, help="dbname")
-  parser.add_argument('-j', '--jira', action="store", dest="jira_issue",
+                      help="dbname")
+  parser.add_argument('-u', '--user', action="store", dest="username",
+                      help="Username to connect")
+  parser.add_argument('--app', action="store", dest="app_name",
+                      help="SAS App name")
+  parser.add_argument('--config', action="store", dest="config_file",
+                      help="CI configuration YAML file")
+  parser.add_argument('--jira', action="store", dest="jira_issue",
                       help="jira ticket issue")
-  args_result = parser.parse_args()
-  main(args_result.dbname, args_result.sql_script, args_result.jira_issue)
+  parser.add_argument('script', metavar='script', type=str, nargs='+',
+                      default=None, help='SQL script to execute')
+  args = parser.parse_args()
+  main(args)
--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
 from __future__ import print_function
@@ -10,16 +9,16 @@ import yaml
 
 def main(argv):
 
-  config_file = 'config.yml'
+  config_file = 'oracle-ci.yml'
 
   with open(config_file) as stream:
       try:
         config = yaml.load(stream)
-        print(config)
+        print(config['variables']['dbname'])
       except yaml.YAMLError as exc:
         print(exc)
         raise
-        
+
 
 if __name__ == "__main__":
   main(sys.argv[1:])
